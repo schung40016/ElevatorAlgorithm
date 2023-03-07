213239ba45ad464105ae8148b248cddca3a52204
schung40016 <55321253+schung40016@users.noreply.github.com>
2023-03-07 13:56:19 -0800

    Completed setters.

--- elevator.cpp
+++ elevator.cpp
@@ -3,8 +3,8 @@
 void Elevator::Print() const
 {
     std::cout << "Elevator currently sits at floor: " << m_currentFloorIndex << "." << std::endl;
-    std::cout << "Elevator is currently " << m_state << "." << std::endl;
-    std::cout << "Elevator is heading " << m_direction << "." << std::endl;
+    std::cout << "Elevator is currently " << static_cast<std::underlying_type<ElevatorState>::type>(m_state) << "." << std::endl;
+    std::cout << "Elevator is heading " << static_cast<std::underlying_type<ElevatorDirection>::type>(m_direction) << "." << std::endl;
 }
 
 // Call to process the elevator.
@@ -12,12 +12,39 @@ void Elevator::Print() const
 // Use switch statement here to determine elevator's state.
 bool Elevator::Tick()
 {
-
+    return true;
 }
 
+// Reset current floor's requests default.
 void Elevator::ClearRequestsOnCurrent()
 {
     m_currentRequests[m_currentFloorIndex].m_downRequest = false;
     m_currentRequests[m_currentFloorIndex].m_stopRequest = false;
     m_currentRequests[m_currentFloorIndex].m_upRequest = false;
+}
+
+void Elevator::SetState(ElevatorState state)
+{
+    m_state = state;
+}
+
+void Elevator::MakeRequest(int floor, Request request)
+{
+    floor -= 1;
+    if (floor > NUM_FLOORS || floor < BASE_FLOOR_INDEX)
+    {
+        std::cout << "Error processing floor input: " << floor << std::endl;
+        return;
+    }
+
+    // Each floor is treated like a state machine
+    switch(request)
+    {
+        case Request::Up: m_currentRequests[floor].m_upRequest = true;
+                        break;
+        case Request::Stop: m_currentRequests[floor].m_stopRequest = true;
+                        break;
+        case Request::Down: m_currentRequests[floor].m_downRequest = true;
+                        break;
+    }
 }
