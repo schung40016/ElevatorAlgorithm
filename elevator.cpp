07ec2f94f5cd75c98ad6027a377c67c362f654a4
schung40016 <55321253+schung40016@users.noreply.github.com>
2023-03-07 13:13:46 -0800

    Restart using Charles' heaer implementation.

--- elevator.cpp
+++ elevator.cpp
@@ -1,123 +1,23 @@
 #include "elevator.h"
 
-Elevator::Elevator(int param_capacity, int param_floorLimit, int param_currFloor)
-    : capacity(param_capacity), floorLimit(param_capacity), currFloor(param_currFloor)
-{   
+void Elevator::Print() const
+{
+    std::cout << "Elevator currently sits at floor: " << m_currentFloorIndex << "." << std::endl;
+    std::cout << "Elevator is currently " << m_state << "." << std::endl;
+    std::cout << "Elevator is heading " << m_direction << "." << std::endl;
 }
 
-// Takes in a elevator request.
-void Elevator::AddRequest(Job& request)
+// Call to process the elevator.
+// Returns true if the elevator has finished processing requests.
+// Use switch statement here to determine elevator's state.
+bool Elevator::Tick()
 {
-    request.SetQueue(jobQueueTracker);
 
-    // Reference the first queue we get.
-    if (upJobLines.size() == 0 && downJobLines.size() == 0 && first == nullptr)
-    {
-        first = &request;   
-        return;
-    }
-
-    if (request.GetDirection() == UP)
-    {
-        upJobLines.emplace(request);
-    }
-    else if (request.GetDirection() == DOWN)
-    {
-        downJobLines.emplace(request);
-    }
-
-    ++jobQueueTracker;
 }
 
-std::queue<Job> Elevator::GetElevatorQueue()
+void Elevator::ClearRequestsOnCurrent()
 {
-    if (first == nullptr)
-    {
-        std::cout << "No requests inserted. Please try again." << std::endl;
-        return jobBank;
-    }
-
-    // Construct and insert our first job.
-    int currDir = this->currFloor < first->GetFloorDestination() ? UP : DOWN;
-    AddQueue(*first, currDir);
-
-    while (upJobLines.size() != 0 || downJobLines.size() != 0)
-    {
-        bool continueUp = jobBank.back().GetCurrentFloor() <= upJobLines.front().GetCurrentFloor();
-        bool continueDown = jobBank.back().GetCurrentFloor() >= downJobLines.front().GetCurrentFloor();
-
-        /*
-        If we are going up and find more queues that are on the way, add them into the main job queue.
-        Vice versa for going down as well.
-        */
-        if ((currDir == UP && continueUp) || downJobLines.empty())
-        {
-            UpdateJobQueue(upJobLines, UP);
-        }
-        else if ((currDir == DOWN && continueDown) || upJobLines.empty())
-        {
-            UpdateJobQueue(downJobLines, DOWN);
-        }
-        else
-        {
-            if (!continueUp && !continueDown)
-            {
-                // Pick the one with the lowest queue number and process it into the main job queue.
-                if (upJobLines.front().GetQueue() < downJobLines.front().GetQueue())
-                {
-                    UpdateJobQueue(upJobLines, UP);
-                    currDir = UP;
-                }
-                else
-                {
-                    UpdateJobQueue(downJobLines, DOWN);
-                    currDir = DOWN;
-                }
-            }
-            else
-            {
-                currDir *= -1;
-            }
-        }
-    }
-
-    return jobBank;
-}
-
-void Elevator::UpdateJobQueue(std::queue<Job>& jobLine, int direction)
-{
-    AddQueue(jobLine.front(), direction);
-    jobLine.pop();
-}
-
-void Elevator::AddQueue(Job& request, int direction)
-{
-    if (this->currFloor == request.GetCurrentFloor() || request.GetInside())
-    {
-        jobBank.push(request);
-    }   
-    else
-    {
-        // Current request is not on the same floor, we create a new request to get to desired request first.
-        Job temp(0, request.GetCurrentFloor(), currFloor, direction, false);  
-        jobBank.push(temp);
-        jobBank.push(request);   
-    }    
-    currFloor = request.GetFloorDestination();
-}
-
-// Resets Elevator for later use.
-void Elevator::ResetElevator()
-{
-    jobQueueTracker = 0;
-    first = nullptr;   
-    EmptyQueue(upJobLines);
-    EmptyQueue(downJobLines);
-    EmptyQueue(jobBank);
-}
-
-void Elevator::EmptyQueue(std::queue<Job>& queue)
-{
-    std::queue<Job> empty;
-    std::swap(queue, empty);
+    m_currentRequests[m_currentFloorIndex].m_downRequest = false;
+    m_currentRequests[m_currentFloorIndex].m_stopRequest = false;
+    m_currentRequests[m_currentFloorIndex].m_upRequest = false;
 }
--- elevator.h
+++ elevator.h
@@ -1,43 +1,68 @@
 #pragma once
 
+#include <assert.h>
 #include <iostream>
-#include <vector>
-#include <queue>
-#include "job.h"
 
-/*
-Elevator Behavior:
-- As the elevator is going down, it will pick up any new requests that are also going down.
-- As the elevator is going up, it will pick up any new requests that are also going up.
-- Once the elevator is finished completed all requests that are going one direction (not all), then it will start 
-  taking orders in the opposite direction. 
-*/ 
+class Elevator {
+public: 
+    enum class ElevatorState : int {
+        Idle,
+        Up,
+        Down
+    };
+    enum class ElevatorDirection : int {
+        Up,
+        Down,
+    };
+    enum class Request : int {
+        Up,
+        Down, 
+        Stop
+    };
+    struct ElevatorRequest {
+        bool m_upRequest = false;
+        bool m_downRequest = false;
+        bool m_stopRequest = false;
+    };
 
-constexpr int UP = 1;
-constexpr int DOWN = -1;
+    // Getters.
+    ElevatorState GetElevatorState() const { return m_state; }
+	const ElevatorRequest& GetRequest(int floor) const { assert(floor > 0 && floor < NUM_FLOORS - 1); return m_currentRequests[floor - 1]; }    // Assert can be used like if statements to stop the program from executing if it is given a wrong input.
+	int GetCurrentFloor() const { return m_currentFloorIndex + 1; }
+	
+	// Check various request states
+	bool HasRequest(const ElevatorRequest& request) const;          // Const ensures that method can't change member variables, only read.
+	bool HasRequestAbove() const;                                 
+	bool HasRequestBelow() const;
+	bool HasRequestOnCurrent() const;
+	int GetNumRequests() const;
+	
+	// Elevator State Setters
+	void ClearRequestsOnCurrent();      
+	void SetState(ElevatorState state);
+	void MakeRequest(int floor, Request request);
+	
+	// Call to process the elevator.
+	// Returns true if the elevator has finished processing requests.
+    // Use switch statement here to determine elevator's state.
+	bool Tick();
 
-class Elevator 
-{
+	// Print elevator status.
+	void Print() const;
+
 private:
-    int capacity = 0;       // Number of people fit in the elevator.
-    int floorLimit = 0;
-    int currFloor = 0;
-    int jobQueueTracker = 0;
-    Job* first = nullptr;
-    std::queue<Job> jobBank = {};
-    std::queue<Job> upJobLines = {};
-    std::queue<Job> downJobLines = {};
+	static constexpr int BASE_FLOOR_INDEX = 0;
+	static constexpr int NUM_FLOORS = 10;
+	static constexpr int NUM_IDLE_TICKS = 5; // How long elevator should stay idle.
+	
+	// Current Floor the elevator is stationed on.
+	int m_currentFloorIndex = BASE_FLOOR_INDEX;
 
-public:
-    Elevator(int param_capacity, int param_floorLimit, int param_currFloor);
+	// NOT A QUEUE/LIST OF REQUESTS. Checks whether each floor has a request like a hash. 
+	ElevatorRequest m_currentRequests[NUM_FLOORS];
 
-    void AddRequest(Job& request);
-
-    std::queue<Job> GetElevatorQueue();
-
-    void UpdateJobQueue(std::queue<Job>& jobLine, int direction);
-
-    void AddQueue(Job& request, int direction);
-
-    void ResetElevator();
+	// State of the elevator
+	ElevatorState m_state = ElevatorState::Idle;
+	ElevatorDirection m_direction = ElevatorDirection::Up;
+	int m_idleTicks = 0;
 };
